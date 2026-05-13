4df306b7d1e3137bda5b275910b0f5d763095497
Ayan21750 <ewrrana785@gmail.com>
2026-05-14 02:53:31 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -38,10 +38,10 @@ module.exports.languages = {
 
 // 🔹 এখানে আপনার ফটো Imgur লিংক করে বসাবেন ✅
 const helpImages = [
-    "https://i.imgur.com/sxSn1K3.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/sxSn1K3.jpeg"
+    "https://files.catbox.moe/z9g88o.jpg",
+    "https://files.catbox.moe/z9g88o.jpg",
+    "https://files.catbox.moe/z9g88o.jpg",
+    "https://files.catbox.moe/z9g88o.jpg"
 ];
 
 
